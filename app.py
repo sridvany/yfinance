33dae9186d51694ed225d3cd9b76231e025b6846
salih rıdvan yılmaz <185217082+sridvany@users.noreply.github.com>
2026-04-05 14:35:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ st.markdown("""
 """, unsafe_allow_html=True)
 
 st.title("📊 tahmin.ai veri indirici")
-st.caption("ne ekersen onu biçersin... | [📊 Çoklu Karşılaştırma](https://verilervs.streamlit.app/)")
+st.caption("ne ekersen onu biçersin...")
 
 # ============================================================
 # Teknik İndikatör Fonksiyonları
